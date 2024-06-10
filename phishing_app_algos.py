dbcfcb48a0c5c6668a1dec04c5e743f062d18a22
Aman | امان <60558994+KORAY-AMAN-ASLAN@users.noreply.github.com>
2024-06-10 15:28:36 +0200

    Update phishing_app_algos.py

--- phishing_app_algos.py
+++ phishing_app_algos.py
@@ -7,51 +7,6 @@ The script demonstrates how to load these models, use them for classification, a
  interface. It is an integration of natural language processing (NLP), machine learning (ML),
 and web development to provide an interactive platform for email classification.
 
-
-
-Key Components and Workflow:
-
-1. Library Imports:
-The script begins by importing necessary libraries for web development (Flask),
-data manipulation (Pandas), NLP (spaCy), machine learning (Scikit-learn, joblib for model loading),
-and visualization (matplotlib for optional plotting).
-
-2. Model Loading:
-Pre-trained models for Logistic Regression,
-Random Forest, and Gradient Boosting are loaded from disk.
-These models are assumed to be trained on a dataset where email texts are labeled as 'legitimate' or 'phishing',
-learning from features extracted via NLP techniques.
-
-3. Email Text Preprocessing:
-A function 'preprocess_text' is defined to clean and normalize the input email texts,
-by removing or replacing URLs and email addresses, optionally filtering out non-alphanumeric characters and stopwords,
-and normalizing the text case.
-This preprocessing mimics the treatment of data during the model training phase and is crucial for making accurate predictions.
-
-4. Data Loading and Preprocessing for Training:
-Another function 'load_and_preprocess_data' automates the loading of new training data from a CSV file,
- applying the preprocessing steps to the 'text' column, and ensuring the 'label' column is in the correct format.
-  This functionality is key for retraining models or evaluating their performance on new data.
-
-5. Model Training with Hyperparameter Optimization:
-The 'train_model' function orchestrates the training of a specified ML model on the preprocessed dataset,
-employing GridSearchCV for hyperparameter tuning to optimize model performance.
-This step is vital for fine-tuning models based on new data or different classification tasks.
-
-6. Flask Web Application Setup:
-Utilizing Flask, the script sets up a web application that can handle GET and POST requests.
-GET requests simply render the homepage, while POST requests accept submitted email texts, preprocess them,
-classify them using the loaded ML models, and return the classification probabilities as phishing or legitimate.
-
-7. Results Visualization and Output:
-Although primarily focused on serving predictions through a web interface,
-the script includes commented sections that suggest how one might extend it to visualize classification results using matplotlib.
-
-8. Execution and Model Management:
-In the script's main block, there's functionality for loading a dataset, training a model, and saving it to disk.
-This illustrates how the web application's underlying ML model,
-can be updated or replaced without modifying the core application logic.
-
 Overall, this script exemplifies a practical application of combining NLP and ML for email classification,
  within a web-based interface, showcasing a pipeline from model training to real-time prediction serving.
 
@@ -167,83 +122,34 @@ def train_model(dataset, model_type='logistic_regression'):
     # Configuration of general and model-specific hyperparameters for optimization using GridSearchCV.
     parameters = {
 
-        # 'max_features' limits the number of features (unique words) considered by the TfidfVectorizer,
-        # helping to reduce the dimensionality of the problem and potentially improving both the performance
-        # and training speed of the model.
-        # The choice of 'max_features' values (3000, 5000, 7000) is designed to explore the balance between model complexity
-        # and computational efficiency. Fewer features make the model faster to train and can help to avoid overfitting by
-        # focusing on the most important words, but too few might miss valuable information. The selected values offer a
-        # range from relatively low to high to find an optimal point.
-        'tfidfvectorizer__max_features': [3000, 5000, 7000],
 
-        # 'ngram_range' specifies the range of n-values for different n-grams to be included. For example,
-        # (1, 2) includes both unigrams and bigrams, allowing the model to consider both individual words
-        # and pairs of consecutive words, which can capture more contextual information than unigrams alone.
-        # The 'ngram_range' is chosen to experiment with the inclusion of not just single words (unigrams) but also combinations
-        # of 2 or 3 consecutive words (bigrams and trigrams). This can enhance the model's understanding of context and phrases,
-        # which might be crucial for the accurate classification of texts. The ranges (1, 2) and (1, 3) allow for comparing the
-        # impact of adding these additional contextual features.
+        'tfidfvectorizer__max_features': [3000, 5000, 7000],
+ 
         'tfidfvectorizer__ngram_range': [(1, 2), (1, 3)]
     }
     model_params = {
-        # For Logistic Regression:
-        # 'C' represents the inverse of regularization strength. Lower values of 'C' specify stronger regularization,
-        # which helps to prevent overfitting by penalizing larger magnitudes of the parameters.
-        # Logistic Regression's 'C' values (0.1, 1, 10) explore the effect of different regularization strengths. A lower 'C'
-        # value means more regularization, discouraging complex models to prevent overfitting, while a higher 'C' allows the
-        # model to become more complex by fitting closely to the training data. These values provide a good range to find the
-        # right balance for the dataset.
         'logistic_regression': {'logisticregression__C': [0.1, 1, 10]},
 
-        # For Random Forest:
-        # 'n_estimators' denotes the number of trees in the forest. Increasing the number of trees can lead to a more
-        # robust model by averaging more decisions, but it also increases the computational load.
-        # For Random Forest and Gradient Boosting, 'n_estimators' are set to 100 and 200 to evaluate how model performance
-        # changes with more trees. More trees generally improve model accuracy but at the cost of increased computational time
-        # and complexity. These values aim to identify a practical trade-off.
+
         'random_forest': {'randomforestclassifier__n_estimators': [100, 200]},
 
-        # For Gradient Boosting:
-        # Similar to Random Forest, 'n_estimators' here specifies the number of boosting stages the model will go through.
-        # More stages can improve the model's accuracy on the training set, but too many can lead to overfitting.
+   
         'gradient_boosting': {'gradientboostingclassifier__n_estimators': [100, 200]},
 
-        # For SVM:
-        # 'C' again controls the strength of regularization where smaller values indicate stronger regularization,
-        # helping to maintain a simpler model that may generalize better. The 'kernel' parameter determines the type
-        # of kernel function used to find the optimal boundary between the classes. 'linear' kernel works well for linearly
-        # separable data, while 'rbf' (radial basis function) can handle non-linear data.
         'svm': {'svc__C': [0.1, 1, 10], 'svc__kernel': ['linear', 'rbf']}
     }
 
     parameters.update(model_params[model_type])
 
 
-    # Performs a grid search to find the best model configuration, optimizing hyperparameters based on accuracy.
     with warnings.catch_warnings():
-        # Temporarily suppresses warnings generated during the grid search. This is useful to keep the output clean
-        # and more readable, especially when certain combinations of hyperparameters might lead to convergence warnings
-        # or other issues that do not necessarily indicate critical failures.
         warnings.simplefilter("ignore")
 
-        # Initializes the GridSearchCV object with the pipeline (which includes the TfidfVectorizer and the selected model),
-        # the combined hyperparameters to test, and specifies cross-validation (cv) with 5 folds. The 'scoring' parameter is
-        # set to 'accuracy', meaning the grid search will evaluate and compare the performance of each model configuration
-        # using the accuracy metric - the proportion of correct predictions out of all predictions made.
-        # This comprehensive search across the specified parameter space aims to identify the combination of hyperparameters
-        # that yields the highest accuracy on the cross-validated training data.
         cv = GridSearchCV(pipeline, parameters, cv=5, scoring='accuracy')
 
-        # Fits the GridSearchCV object to the training data. This step involves training multiple versions of the model
-        # with different combinations of the specified hyperparameters. Each combination is evaluated using cross-validation,
-        # where the training set is split into 'cv' smaller sets, the model is trained on 'cv'-1 of those sets and validated
-        # on the remaining set. This process is repeated 'cv' times, each time with a different set held out for validation,
-        # to ensure that the performance estimate is robust and less dependent on the particular way the data is split.
-        # The best performing model configuration (according to accuracy) is identified and retained for future predictions.
         cv.fit(X_train, y_train)
 
 
-    # Outputs the best hyperparameter settings and evaluates the tuned model on the test dataset.
     print(f"Best parameters: {cv.best_params_}")
     y_pred = cv.predict(X_test)
     print(classification_report(y_test, y_pred))
@@ -274,7 +180,6 @@ def index():
             prediction_prob = model.predict_proba([preprocessed_text])[0]
             phishing_prob = prediction_prob[1] * 100
 
-            # Return the prediction in the appropriate format based on the request type.
             return jsonify(probability="{:.2f}".format(phishing_prob)) if request.is_json else render_template(
                 'index.html', probability="{:.2f}%".format(phishing_prob))
         except Exception as e:
@@ -286,13 +191,4 @@ def index():
 
 
 if __name__ == '__main__':
-    # Train and save the model, or load an existing model, then run the Flask app.
-    # This section can be commented out or modified based on the deployment strategy to avoid retraining on every launch.
-    # dataset = load_and_preprocess_data('data.csv')
-    # logistic_regression_model.pkl
-    # mrandom_forest_model.pkl
-    # gradient_boosting_model.pkl
-
-   # model = train_model(dataset, 'logistic_regression')
-    #joblib.dump(model, MODEL_PATH)
     app.run(debug=True)
